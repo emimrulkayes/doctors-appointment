c39d462f7be9e9f687ec83218f919099eb8e7826
emimrulkayes <emimrulkayes@gmail.com>
2023-02-27 14:00:39 +0600

    Setup Routes

---
+++ src/Pages/Routes/Routes/Routes.js
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from 'react-router-dom';
+import About from '../../About/About';
+import Appointment from '../../Appointment/Appointment';
+import Contact from '../../Contact/Contact';
+import Home from '../../Home/Home';
+import Main from '../../Layout/Main';
+import Login from '../../Login/Login';
+import Reviews from '../../Reviews/Reviews';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: '/appointment',
+                element: <Appointment/>,
+            },
+            {
+                path: '/about',
+                element: <About />,
+
+            },
+            {
+                path: '/reviews',
+                element: <Reviews />,
+
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+
+
+        ]
+    }
+])
