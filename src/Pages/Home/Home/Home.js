4e1bb8ec7ab1007f1c54d8f02641185313eb45e4
imrul-xs <107181705+imrul-xs@users.noreply.github.com>
2023-04-21 03:57:20 +0600

    Added Cards and Services Section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Cards from '../Cards/Cards';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
+            <Banner />
+            <Cards />
+            <Services />
         </div>
     );
 };
