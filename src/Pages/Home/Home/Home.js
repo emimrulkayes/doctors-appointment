4f174d1bf5f5181ca1b48563079ffc525c148a06
imrul-xs <107181705+imrul-xs@users.noreply.github.com>
2023-04-21 16:11:44 +0600

    Added Appoinment

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Cards from '../Cards/Cards';
 import Services from '../Services/Services';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <Cards />
             <Services />
+            <MakeAppointment />
         </div>
     );
 };
